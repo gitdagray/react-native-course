9afd4f86728f3a46ad704ffa89a12a12f1120d8a
gitdagray <DaveOnEleven@gmail.com>
2024-10-09 14:48:48 -0500

    Completed lesson02

---
+++ MyApp/app/(tabs)/index.jsx
@@ -0,0 +1,40 @@
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+
+import icedCoffeeImg from "@/assets/images/iced-coffee.png"
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={icedCoffeeImg}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>Coffee Shop</Text>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  }
+})
